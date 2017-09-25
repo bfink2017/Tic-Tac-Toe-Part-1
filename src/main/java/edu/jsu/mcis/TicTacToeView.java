0154eabba87a00702ca63726386766ef68ef8781
Brendan Fink <bfink@stu.jsu.edu>
2017-09-25 07:22:57 -0500

    Part 2 final commit

--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -1,56 +1,117 @@
 package edu.jsu.mcis;
 
-public class TicTacToeView {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-    private TicTacToeModel model;
+public class TicTacToeView extends JPanel implements ActionListener {
     
-    /* CONSTRUCTOR */
-	
+    TicTacToeModel model;
+
+    private JButton[][] squares;
+    private JPanel squaresPanel;
+    private JLabel resultLabel;
+	private int width;
+
     public TicTacToeView(TicTacToeModel model) {
-        
+
         this.model = model;
+
+        width = model.getWidth();
+
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        squares = new JButton[width][width];
+        squaresPanel = new JPanel(new GridLayout(width,width));
+        resultLabel = new JLabel();
+        resultLabel.setName("ResultLabel");
+        
+        /* Loop through all rows and columns of the grid and: create a new
+           JButton and assign it to the current grid element, add "this"
+           (the View object) as the ActionListener, set the JButton's name
+           to "SquareXY" (where X is the row and Y is the column), set the
+           preferred size to 64x64 pixels, and add the new square to the
+           "squaresPanel" JPanel created earlier. */
+        
+        for (int i = 0; i < width; i++) {
+			for (int j = 0; j < width; j++) {
+				squares[i][j] = new JButton();
+				squares[i][j].setName("Square"+i+j);
+				squares[i][j].addActionListener(this);
+				squares[i][j].setText("-");
+				squares[i][j].setPreferredSize(new Dimension(60,60));
+				squaresPanel.add(squares[i][j]);
+			
+			}
+		}
+        
+        add(squaresPanel);
+        add(resultLabel);
+        
+        resultLabel.setText("Welcome to Tic-Tac-Toe!");
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        
+        String name = ((JButton) event.getSource()).getName();
+        
+        /* Extract the ROW and COL from the string name (remember, the squares
+           should all be named "SquareXY", so the ROW should be extracted from
+           position 6 of this string, and the COL from position 7.  Remember to
+           convert both to Integers!) */
+
+		char x = name.charAt(6);
+		char y = name.charAt(7);
+		   
+        int ROW = Character.getNumericValue(x);
+		int COL = Character.getNumericValue(y);
+           
+        /* Call makeMark() to place the mark in the Model */
+        
+        model.makeMark(ROW, COL);
+
+        /* Update the squares of the View using the "updateSquares()" method
+           (see below); this is the equivalent of re-printing the grid to the
+           console in Part One. */
+
+        updateSquares();
+
+        /* Clear the "showResult" label at the bottom of the View */
+        
+        showResult(" ");
+        
+        /* If the game is over (that is, if the "getResult()" method returns
+           a result other than Result.NONE), loop through all the JButtons in
+           the View and disable them to prevent further entries.  (Hint: the
+           JButton class provides a "setEnabled()" method for this.) */
+           
+        if (model.getResult().toString() != "none") {
+			for (int i = 0; i < width; i++) {
+				for (int j = 0; j < width; j++) {
+				squares[i][j].setEnabled(false);
+				}
+			}
+		}
         
     }
-	
-    public void viewModel() {
         
-        /* Print the board to the console (see examples) */
-       
-	   System.out.println("\n");
-       System.out.println("012");
-	   System.out.println("\n");
-	   System.out.println("0" + model.getMark(0, 0) + model.getMark(0, 1) + model.getMark(0, 2));
-	   System.out.println("1" + model.getMark(1, 0) + model.getMark(1, 1) + model.getMark(1, 2));
-	   System.out.println("2" + model.getMark(2, 0) + model.getMark(2, 1) + model.getMark(2, 2));
-    }
+    public void updateSquares() {
 
-    public void showNextMovePrompt() {
-
-        /* Display a prompt for the player's next move (see examples) */
-
-        if (model.isXTurn() == true) {
-			System.out.println("Player 1(X)Move: ");
-		}
-		else {
-			System.out.println("Player 2(O)Move: ");
+        /* Loop through all of the rows and columns in the JButton grid and
+           reset the button text to match the corresponding marks as returned
+           by "getMark()" (to update the View to match the Model) */
+           
+         for (int i = 0; i < width; i++) {
+			for (int j = 0; j < width; j++) {
+				squares[i][j].setText(model.getMark(i, j).toString());
+			}
 		}
 
     }
-
-    public void showInputError() {
-
-        /* Display an error if input is invalid (see examples) */
-
-        System.out.println("Invalid input!");
-
+        
+    public void showResult(String message) {
+        resultLabel.setText(message);
     }
 
-    public void showResult(String r) {
-
-        /* Display final winner */
-
-        System.out.println(r + "!");
-
-    }
-	
 }
